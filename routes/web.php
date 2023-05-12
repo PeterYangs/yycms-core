82bd545d16d83c7bee16d7658f2f21df3b2ac597
peter <904801074@qq.com>
2023-05-12 17:06:28 +0800

    图片访问优化

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,60 @@
 <?php
 
 
-Route::get('/backend/{path}.{ex}',function (){
+//后台图片访问
+Route::get('/backend/{path}.{ex}', function ($path, $ex) {
 
 
-    return "123";
+    switch (env('UPLOAD_TYPE')) {
 
-})->where(['path'=>"[/A-Za-z0-9.]+",'ex'=>"(jpg|jpeg|png|gif|webp){1}"]);
+
+        case "ali_oss":
+
+
+            return response()->redirectGuest(env('IMAGE_DOMAIN') . "/uploads/" . $path . "." . $ex);
+
+
+        default:
+
+            if (Storage::disk('upload')->exists($path . "." . $ex)) {
+
+                return response()->file(Storage::disk('upload')->path($path . "." . $ex));
+            }
+
+            abort(404);
+
+    }
+
+
+})->where(['path' => "[/A-Za-z0-9._]+", 'ex' => "(jpg|jpeg|png|gif|webp){1}"]);
+
+
+//编辑器图片访问
+Route::get("/api/uploads/{path}.{ex}", function ($path, $ex) {
+
+
+
+    switch (env('UPLOAD_TYPE')) {
+
+
+        case "ali_oss":
+
+
+            return response()->redirectGuest(env('IMAGE_DOMAIN') . "/uploads/" . $path . "." . $ex);
+
+
+        default:
+
+            if (Storage::disk('upload')->exists($path . "." . $ex)) {
+
+                return response()->file(Storage::disk('upload')->path($path . "." . $ex));
+            }
+
+            abort(404);
+
+    }
+
+
+})->where(['path' => "[/A-Za-z0-9._]+", 'ex' => "(jpg|jpeg|png|gif|webp){1}"]);
+
+
