17e7cca0e556e789878d8ca28ac3924f63ff0d00
peter <904801074@qq.com>
2023-06-05 16:00:21 +0800

    seo优化

--- Http/View/_seo.blade.php
+++ Http/View/_seo.blade.php
@@ -1,13 +1,13 @@
 @if(request()->route('_type') === "channel" && isset($category) && $category instanceof \Ycore\Models\Category)
-    <title>{{getSeoTitleForChannel($category)}}</title>
+    <title>{{getSeoTitleForChannel($category)}}-{{getOption('site_name')}}</title>
     <meta name="keywords" content="{{getSeoKeywordForChannel($category)}}"/>
     <meta name="description" content="{{getSeoDescForChannel($category)}}"/>
 @elseif(request()->route('_type') === "detail" && isset($data) && $data instanceof \Ycore\Models\Article)
-    <title>{{getSeoTitleForDetail($data)}}</title>
+    <title>{{getSeoTitleForDetail($data)}}-{{getOption('site_name')}}</title>
     <meta name="keywords" content="{{getSeoKeywordForDetail($data)}}"/>
     <meta name="description" content="{{getSeoDescForDetail($data)}}"/>
 @else
-    @if(isset($_seo_title)&&$_seo_title!=="")<title>{{$_seo_title}}</title>@else<title>{{getOption('seo_title')}}</title>@endif
+    @if(isset($_seo_title)&&$_seo_title!=="")<title>{{$_seo_title}}-{{getOption('site_name')}}</title>@else<title>{{getOption('seo_title')}}-{{getOption('site_name')}}</title>@endif
 
     @if(isset($_seo_keyword)&&$_seo_keyword!=="")<meta name="keywords" content="{{$_seo_keyword}}"/>@else<meta name="keywords" content="{{getOption('seo_keyword')}}"/>@endif
 
