83dd283d6ac3f40a330c71d99be139333273e767
peter <904801074@qq.com>
2023-06-13 17:34:55 +0800

    时间因子优化

--- Http/View/_common.blade.php
+++ Http/View/_common.blade.php
@@ -168,7 +168,7 @@
     <script type="application/ld+json">
     {
         "@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld",
-        "@id": "{{request()->fullUrl()}}",
+        "@id": "{{request()->fullUrlWithoutQuery('admin_key')}}",
         "title": "{{getOption('seo_title')}}-{{getOption('site_name')}}",
         "description": "{{getOption('seo_desc')}}",
         "pubDate": "{{date("Y-m-d\TH:i:s",strtotime(optional(ArticleListModel()->orderBy('push_time','asc')->first())->push_time))}}",
