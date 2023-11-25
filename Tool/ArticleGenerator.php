4fad241f912698945599d86c3f54cf2150b795b2
peter <904801074@qq.com>
2023-11-25 21:35:45 +0800

    分发优化

--- Tool/ArticleGenerator.php
+++ Tool/ArticleGenerator.php
@@ -558,4 +558,47 @@ class ArticleGenerator
     }
 
 
+    function articleStatic(int $articleId): bool
+    {
+
+        $article = Article::where('id', $articleId)->with('category')->first();
+
+
+        if (!$article) {
+
+
+            return false;
+        }
+
+        try {
+
+            $b = \Http::get(getDetailUrlForCli($article) . '?admin_key=' . env('ADMIN_KEY'))->body();
+
+
+            \Storage::disk('static')->put('pc/' . str_replace("{id}", $article->id,
+                    \Cache::get('category:detail:pc_' . $article->category->id)),
+                $b);
+
+
+            $b = \Http::withHeaders(['User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'])->get(getDetailUrlForCli($article,
+                    'mobile') . '?admin_key=' . env('ADMIN_KEY'))->body();
+
+
+            \Storage::disk('static')->put('mobile/' . str_replace("{id}", $article->id,
+                    \Cache::get('category:detail:mobile_' . $article->category->id)), $b);
+
+
+        } catch (\Exception $exception) {
+
+
+            \Log::error("文章静态化失败，文章id为" . $article->id . "(" . $exception->getMessage() . ")");
+
+            return false;
+
+        }
+
+        return true;
+    }
+
+
 }
